2c96be74e559fb456d31e6deb6c09f2765c8dcc7
prozorovmy <miha.prozorov1@gmail.com>
2024-11-20 13:07:42 +0200

    Added class InitDb

--- src/main/java/ru/kata/spring/boot_security/demo/entity/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entity/User.java
@@ -13,37 +13,51 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.List;
 
-@Data
-@NoArgsConstructor(access = AccessLevel.PUBLIC)
-@AllArgsConstructor
 @Entity
+@Data
 @Table(name = "users")
 public class User implements UserDetails {
 
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Size(min=2, message = "Не меньше 5 знаков")
+    @Size(min = 2, message = "Не меньше 5 знаков")
     @Column(name = "username")
     private String username;
 
     @Column(name = "email")
     private String email;
 
-    @Size(min=2, message = "Не меньше 5 знаков")
+    @Size(min = 2, message = "Не меньше 5 знаков")
     @Column(name = "password")
     private String password;
 
     @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
     private Collection<Role> roles;
 
+    public User() {
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
---
+++ src/main/java/ru/kata/spring/boot_security/demo/initializer/InitDb.java
@@ -0,0 +1,53 @@
+package ru.kata.spring.boot_security.demo.initializer;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.demo.entity.Role;
+import ru.kata.spring.boot_security.demo.entity.User;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+
+@Component
+public class InitDb {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+
+    @Autowired
+    public InitDb(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @PostConstruct
+    public void init() {
+
+        // Создаем роли
+        Role adminRole = new Role();
+        adminRole.setRoleName("ROLE_ADMIN");
+        roleRepository.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setRoleName("ROLE_USER");
+        roleRepository.save(userRole);
+
+        // Создаем пользователей
+        User admin = new User("admin", "admin@example.com", "$2a$12$KUWvPmLUso.yacf.vsfWTOLt9r8GXjkFlNUio4Dt7ISxoHeHVQHAe");
+        admin.setRoles(List.of(adminRole, userRole));
+
+        User user = new User("user", "user@example.com","$2a$12$KUWvPmLUso.yacf.vsfWTOLt9r8GXjkFlNUio4Dt7ISxoHeHVQHAe");
+        user.setRoles(List.of(userRole));
+
+        // Сохраняем пользователей
+        userRepository.save(admin);
+        userRepository.save(user);
+
+        System.out.println("Тестовые пользователи и роли успешно добавлены в БД.");
+
+    }
+}

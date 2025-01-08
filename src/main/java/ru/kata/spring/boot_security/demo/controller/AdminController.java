5bec74f98434a9249834d05cc48890ffdcc43d8c
prozorovmy <miha.prozorov1@gmail.com>
2025-01-08 23:12:28 +0300

    Сделал отельный класс для обработки исключений

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -5,17 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.entity.Role;
 import ru.kata.spring.boot_security.demo.entity.User;
@@ -140,19 +136,5 @@ public class AdminController {
         model.addAttribute("currentUserByAdmin", user);
 
         return "7_user-information-page";
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<UserErrorResponse> handlerException (UserNotFoundException e){
-        UserErrorResponse userErrorResponse = new UserErrorResponse();
-        userErrorResponse.setMessage(e.getMessage());
-        return new ResponseEntity<>(userErrorResponse, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<UserErrorResponse> handlerException (Exception e){
-        UserErrorResponse userErrorResponse = new UserErrorResponse();
-        userErrorResponse.setMessage(e.getMessage());
-        return new ResponseEntity<>(userErrorResponse, HttpStatus.BAD_REQUEST);
     }
 }

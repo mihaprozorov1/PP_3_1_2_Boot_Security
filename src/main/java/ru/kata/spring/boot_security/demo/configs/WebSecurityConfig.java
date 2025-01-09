afb062f14f64bd42f5f83a9912cadeb7be5f0d2a
prozorovmy <miha.prozorov1@gmail.com>
2025-01-09 18:42:19 +0300

    Переделал метод в контроллере на рестовый. Теперь прокидываю json в метод создается USER. Добавил в конфиг настройку секьюрности - permitAll - для прокидывания запросов в POSTMAN

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -27,28 +27,47 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
-                .authorizeRequests()//
-                .antMatchers("/").permitAll()//Любой пользователь, независимо от роли, имеет доступ к корневому пути /.
-                .antMatchers("/403").permitAll()
-                .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/admin/**").hasAnyRole("ADMIN")
-                .anyRequest().authenticated() // любой запрос, который не подходит под предыдущие правила, требует аутентификации.
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/admin/**").permitAll() // API доступен для всех
+                .antMatchers("/user/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .usernameParameter("email")
-                .successHandler(successUserHandler)//.formLogin() — активирует стандартную форму входа Spring Security.
-                //.successHandler(successUserHandler) — это обработчик, который будет вызван после успешной аутентификации. Он, вероятно,
-                // выполняет дополнительные действия, например, перенаправление пользователя на определенную страницу или запись в лог.
-                .permitAll()//.permitAll() — указывает, что доступ к странице входа (обычно /login) открыт для всех пользователей.
+                .successHandler(successUserHandler)
+                .permitAll()
                 .and()
-                .logout().permitAll() //Выход доступен всем пользователям.
+                .logout().permitAll()
                 .and()
-                .exceptionHandling().accessDeniedPage("/403");//Если пользователь пытается получить доступ к ресурсу,
-        // к которому у него нет прав, он будет перенаправлен на страницу /403.
+                .exceptionHandling().accessDeniedPage("/403");
     }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf()
+//                .disable()
+//                .authorizeRequests()//
+//                .antMatchers("/").permitAll()//Любой пользователь, независимо от роли, имеет доступ к корневому пути /.
+//                .antMatchers("/403").permitAll()
+//                .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+//                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+//                .anyRequest().authenticated() // любой запрос, который не подходит под предыдущие правила, требует аутентификации.
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .usernameParameter("email")
+//                .successHandler(successUserHandler)//.formLogin() — активирует стандартную форму входа Spring Security.
+//                //.successHandler(successUserHandler) — это обработчик, который будет вызван после успешной аутентификации. Он, вероятно,
+//                // выполняет дополнительные действия, например, перенаправление пользователя на определенную страницу или запись в лог.
+//                .permitAll()//.permitAll() — указывает, что доступ к странице входа (обычно /login) открыт для всех пользователей.
+//                .and()
+//                .logout().permitAll() //Выход доступен всем пользователям.
+//                .and()
+//                .exceptionHandling().accessDeniedPage("/403");//Если пользователь пытается получить доступ к ресурсу,
+//        // к которому у него нет прав, он будет перенаправлен на страницу /403.
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,22 +1,18 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.entity.Role;
 import ru.kata.spring.boot_security.demo.entity.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
-import ru.kata.spring.boot_security.demo.utill.UserErrorResponse;
 import ru.kata.spring.boot_security.demo.utill.UserNotFoundException;
 
 import java.util.HashSet;
@@ -34,54 +30,30 @@ public class AdminController {
         this.userService = userService;
     }
 
-
+    //	Вывести всех Юзеров
     @GetMapping({"", "/"})
-    public  List<User> getAllUser(Model model) {
+    public List<User> getAllUser(Model model) {
         return userService.listUsers();//return "users";
     }
 
+    //	Вывести Юзера по ID
     @GetMapping("/getById") //Обрабатывает GET-запросы с параметром id (например, /admin/1).
     public User show(@RequestParam("id") int id) { //Извлекает параметр id из запроса.
         if (userService.getById(id) == null) {
-            throw new UserNotFoundException("User с таким ID = " + id + " в базе-данных нет" );
+            throw new UserNotFoundException("User с таким ID = " + id + " в базе-данных нет");
         }
         return userService.getById(id);
     }
 
     //	Добавить Юзера
-    @GetMapping("/3_add-new-user")
-//    @GetMapping("/new")
-    public String newUser(@ModelAttribute("user") User user) {
-        return "3_add-new-user";
-//         return "new";
-    }
-    //@GetMapping("/new"): Обрабатывает GET-запрос к /admin/new.
-    //@ModelAttribute("user") User user: Создаёт пустой объект User и добавляет его в модель под именем user.
-    //return "new";: Возвращает имя представления new.
-
-    //    @PostMapping("/new")
-//    @PostMapping("/3_add-new-user")
-//    public String create(@ModelAttribute("user") User user, @RequestParam("role") String roleName) {
-//        Role role = userService.findRoleByName(roleName);
-//        user.setRoles(Set.of(role));// Устанавливаем новую роль
-//        userService.save(user);// Сохраняем изменения
-//        return "redirect:/admin";
-//    }
-    //@PostMapping("/new"): Обрабатывает POST-запросы к /admin/new.
-    //@ModelAttribute("user") User user: Извлекает данные из формы и связывает их с объектом User.
-    //@RequestParam("role"): Извлекает выбранную роль из формы.
-    //userService.findRoleByName(roleName): Получает объект роли по её имени.
-    //user.setRoles(Set.of(role)): Устанавливает выбранную роль для пользователя.
-    //userService.save(user): Сохраняет нового пользователя.
-    //return "redirect:/admin";: Перенаправляет пользователя обратно на страницу /admin.
-
-    @PostMapping("/3_add-new-user")
-    public String create(@ModelAttribute("user") User user, @RequestParam("role") String roleName) {
+    @PostMapping("/create")
+    public User create(@RequestBody User user, @RequestParam("role") String roleName) {
         Role role = userService.findRoleByName(roleName);
         user.setRoles(Set.of(role));// Устанавливаем новую роль
-        userService.save(user);// Сохраняем изменения
-        return "redirect:/admin";
+        userService.save(user);
+        return user;
     }
+
     //  Изменить Юзера
     @GetMapping("/edit")
     public String edit(Model model, @RequestParam("id") int id) {
@@ -89,10 +61,6 @@ public class AdminController {
         model.addAttribute("roles", userService.getAllRoles());
         return "edit";
     }
-    //@GetMapping("/edit"): Обрабатывает GET-запросы к /admin/edit.
-    //@RequestParam("id") int id: Извлекает параметр id из запроса.
-    //model.addAttribute("user", userService.getById(id)): Добавляет пользователя с данным ID в модель.
-    //return "edit";: Возвращает имя представления edit.
 
     @PostMapping("/edit")
     public String update(Long userId, String firstName, String lastName, Integer age, String email, String role) {
@@ -107,21 +75,13 @@ public class AdminController {
         userService.edit(user); // Обновляем данные пользователя
         return "redirect:/admin";
     }
-    //@PostMapping("/edit"): Обрабатывает POST-запросы к /admin/edit.
-    //@ModelAttribute("user") User user: Извлекает данные из формы и связывает их с объектом User.
-    //userService.edit(user): Обновляет данные пользователя.
-    //return "redirect:/admin";: Перенаправляет пользователя обратно на страницу /admin.
 
     //   Удалить Юзера
-    @GetMapping( "/delete")
+    @GetMapping("/delete")
     public String deleteUser(@RequestParam("userId") int id) {
         userService.delete(id);
         return "redirect:/admin";
     }
-    //@RequestMapping(value = "/delete", method = RequestMethod.GET): Обрабатывает GET-запросы к /admin/delete.
-    //@RequestParam("id") int id: Извлекает параметр id из запроса.
-    //userService.delete(id): Удаляет пользователя по его ID.
-    //return "redirect:/admin";: Перенаправляет пользователя обратно на страницу /admin.
 
     @GetMapping("/7_user-information-page")
     public String show(Model model) {
